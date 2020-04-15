47e0acf69754294cb813529a4db5e3979a6d8d09
achmadsatyap <satyapradana98@gmail.com>
2020-04-15 16:34:46 +0700

    Tugas selesai

---
+++ satyapradana-minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default{
+        apiKey: "AIzaSyBOmEGCI6gztid-Aqf_iFxR67wi7hv1kig",
+        authDomain: "pbfminggu11-47c68.firebaseapp.com",
+        databaseURL: "https://pbfminggu11-47c68.firebaseio.com",
+        projectId: "pbfminggu11-47c68",
+        storageBucket: "pbfminggu11-47c68.appspot.com",
+        messagingSenderId: "325687332431",
+        appId: "1:325687332431:web:ec193680999d08e803243a",
+        measurementId: "G-DEY5Q5C390"
+};
