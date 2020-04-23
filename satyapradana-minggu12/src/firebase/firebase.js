22dcd2d0865898bce7165615d86695e73f621feb
achmadsatyap <satyapradana98@gmail.com>
2020-04-23 11:48:52 +0700

    Tugas Selesai

---
+++ satyapradana-minggu12/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+        apiKey: "AIzaSyCWtwLkJnACDS8Kug1Ai5gxvijjMSk5_QM",
+        authDomain: "fir-login-b767a.firebaseapp.com",
+        databaseURL: "https://fir-login-b767a.firebaseio.com",
+        projectId: "fir-login-b767a",
+        storageBucket: "fir-login-b767a.appspot.com",
+        messagingSenderId: "630418970404",
+        appId: "1:630418970404:web:bc4427d989ceacf37045b1",
+        measurementId: "G-F00CKVL6XJ"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
