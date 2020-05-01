e19249831743f18fbc2c6f1851838f44ec5de209
achmadsatyap <satyapradana98@gmail.com>
2020-05-01 17:15:11 +0700

    Done

---
+++ satyapradana-minggu13/satyapradana-praktikum/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWtwLkJnACDS8Kug1Ai5gxvijjMSk5_QM",
+    authDomain: "fir-login-b767a.firebaseapp.com",
+    databaseURL: "https://fir-login-b767a.firebaseio.com",
+    projectId: "fir-login-b767a",
+    storageBucket: "fir-login-b767a.appspot.com",
+    messagingSenderId: "630418970404",
+    appId: "1:630418970404:web:bc4427d989ceacf37045b1",
+    measurementId: "G-F00CKVL6XJ"
+  };
+  export default firebaseConfig;
